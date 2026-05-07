f336f7d4b0b3742119e78870b21331ac0667e789
Harsh Choudhary <harshchoudhary50226@gmail.com>
2026-05-07 11:57:55 +0530

    Delete Pixora directory

--- Pixora/src/main/java/com/harsh/Pixora/ingestion/ImageProcessingClient.java
+++
@@ -1,9 +0,0 @@
-package com.harsh.Pixora.ingestion;
-
-import java.nio.file.Path;
-import java.util.List;
-import java.util.Map;
-
-public interface ImageProcessingClient {
-    List<Map<String, Object>> processBatch(List<Path> imagePaths);
-}
